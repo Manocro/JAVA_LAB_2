145f0a6b7fa2412bedca3b0f87df7e729b06c503
Manocro <manocro5555@gmail.com>
2025-02-18 21:04:17 -0600

    Fixes button saying Add.LOGIC.Event to just Add Event

--- src/GUI/EventPlanner.java
+++ src/GUI/EventPlanner.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class EventPlanner {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("LOGIC.Event Planner");
+            JFrame frame = new JFrame("Event Planner");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(800, 600);
 
